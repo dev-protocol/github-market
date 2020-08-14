35d3dd75865541e2568aba2a5ef5cd3fa2bab453
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-08-14 14:19:53 +0900

    d

--- contracts/GitHubMarket.sol
+++ contracts/GitHubMarket.sol
@@ -5,167 +5,171 @@ pragma experimental ABIEncoderV2;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IMarketBehavior {
-	function authenticate(
-		address _prop,
-		string calldata _args1,
-		string calldata _args2,
-		string calldata _args3,
-		string calldata _args4,
-		string calldata _args5,
-		address market
-	)
-		external
-		returns (
-			// solium-disable-next-line indentation
-			bool
-		);
+    function authenticate(
+        address _prop,
+        string calldata _args1,
+        string calldata _args2,
+        string calldata _args3,
+        string calldata _args4,
+        string calldata _args5,
+        address market
+    )
+        external
+        returns (
+            // solium-disable-next-line indentation
+            bool
+        );
 
-	function schema() external view returns (string memory);
+    function schema() external view returns (string memory);
 
-	function getId(address _metrics) external view returns (string memory);
+    function getId(address _metrics) external view returns (string memory);
 
-	function getMetrics(string calldata _id) external view returns (address);
+    function getMetrics(string calldata _id) external view returns (address);
 }
-
-
 
 interface IMarket {
-	function authenticate(
-		address _prop,
-		string calldata _args1,
-		string calldata _args2,
-		string calldata _args3,
-		string calldata _args4,
-		string calldata _args5
-	)
-		external
-		returns (
-			// solium-disable-next-line indentation
-			bool
-		);
+    function authenticate(
+        address _prop,
+        string calldata _args1,
+        string calldata _args2,
+        string calldata _args3,
+        string calldata _args4,
+        string calldata _args5
+    )
+        external
+        returns (
+            // solium-disable-next-line indentation
+            bool
+        );
 
-	function authenticatedCallback(address _property, bytes32 _idHash)
-		external
-		returns (address);
+    function authenticatedCallback(address _property, bytes32 _idHash)
+        external
+        returns (address);
 
-	function deauthenticate(address _metrics) external;
+    function deauthenticate(address _metrics) external;
 
-	function schema() external view returns (string memory);
+    function schema() external view returns (string memory);
 
-	function behavior() external view returns (address);
+    function behavior() external view returns (address);
 
-	function enabled() external view returns (bool);
+    function enabled() external view returns (bool);
 
-	function votingEndBlockNumber() external view returns (uint256);
+    function votingEndBlockNumber() external view returns (uint256);
 
-	function toEnable() external;
+    function toEnable() external;
 }
-
 
 contract GitHubMarket is IMarketBehavior, Ownable {
-	address private khaos;
-	string public schema = "['GitHub package', 'GitHub token']";
-	bool public migratable = true;
+    address private khaos;
+    string public schema = "['GitHub package', 'GitHub token']";
+    bool public migratable = true;
 
-	mapping(address => string) private packages;
-	mapping(bytes32 => address) private metrics;
-	mapping(bytes32 => bool) private pendingAuthentication;
-	event Registered(address _metrics, string _package);
-	event Authenticated(QueryData _data);
-	event Query(bytes _data);
-	struct QueryData {
-		bytes32 key;
-		string package;
-		string publicSignature;
-		address callback;
-		address property;
-	}
-	function authenticate(
-		address _prop,
-		string memory _githubPackage,
-		string memory _publicSignature,
-		string memory,
-		string memory,
-		string memory,
-		address _dest
-	) public override returns (bool) {
-		bytes32 key = createKey(_githubPackage);
-		QueryData memory d = QueryData(key, _githubPackage, _publicSignature, _dest, _prop);
-		emit Query(abi.encode(d));
+    mapping(address => string) private packages;
+    mapping(bytes32 => address) private metrics;
+    mapping(bytes32 => bool) private pendingAuthentication;
+    event Registered(address _metrics, string _package);
+    event Authenticated(QueryData _data);
+    event Query(bytes _data);
+    struct QueryData {
+        bytes32 key;
+        string package;
+        string publicSignature;
+        address callback;
+        address property;
+    }
 
-		pendingAuthentication[key] = true;
-		return true;
-	}
+    function authenticate(
+        address _prop,
+        string memory _githubPackage,
+        string memory _publicSignature,
+        string memory,
+        string memory,
+        string memory,
+        address _dest
+    ) public override returns (bool) {
+        bytes32 key = createKey(_githubPackage);
+        QueryData memory d = QueryData(
+            key,
+            _githubPackage,
+            _publicSignature,
+            _dest,
+            _prop
+        );
+        emit Query(abi.encode(d));
 
-	function khaosCallback(bytes memory _data) external {
-		require(msg.sender == khaos, "illegal access");
-		QueryData memory callback = abi.decode(
-            _data,
-            (QueryData)
-		);
-		emit Authenticated(callback);
-		require(pendingAuthentication[callback.key], "illegal data");
-		delete pendingAuthentication[callback.key];
-		register(callback.key, callback.property, callback.package, callback.callback);
-	}
+        pendingAuthentication[key] = true;
+        return true;
+    }
 
-	function register(
-		bytes32 _key,
-		address _property,
-		string memory _package,
-		address _market
-	) private {
-		address _metrics = IMarket(_market).authenticatedCallback(
-			_property,
-			_key
-		);
-		packages[_metrics] = _package;
-		metrics[_key] = _metrics;
-		emit Registered(_metrics, _package);
-	}
+    function khaosCallback(bytes memory _data) external {
+        require(msg.sender == khaos, "illegal access");
+        QueryData memory callback = abi.decode(_data, (QueryData));
+        emit Authenticated(callback);
+        require(pendingAuthentication[callback.key], "illegal data");
+        delete pendingAuthentication[callback.key];
+        register(
+            callback.key,
+            callback.property,
+            callback.package,
+            callback.callback
+        );
+    }
 
-	function createKey(string memory _package) private pure returns (bytes32) {
-		return keccak256(abi.encodePacked(_package));
-	}
+    function register(
+        bytes32 _key,
+        address _property,
+        string memory _package,
+        address _market
+    ) private {
+        address _metrics = IMarket(_market).authenticatedCallback(
+            _property,
+            _key
+        );
+        packages[_metrics] = _package;
+        metrics[_key] = _metrics;
+        emit Registered(_metrics, _package);
+    }
 
-	function getId(address _metrics)
-		public
-		view override
-		returns (string memory)
-	{
-		return packages[_metrics];
-	}
+    function createKey(string memory _package) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_package));
+    }
 
-	function getMetrics(string memory _package)
-		public
-		view override
-		returns (address)
-	{
-		return metrics[createKey(_package)];
-	}
+    function getId(address _metrics)
+        public
+        override
+        view
+        returns (string memory)
+    {
+        return packages[_metrics];
+    }
 
-	function migrate(
-		address _property,
-		string memory _package,
-		address _market
-	) public onlyOwner {
-		bytes32 key = createKey(_package);
-		require(migratable, "now is not migratable");
-		register(key, _property, _package, _market);
-	}
+    function getMetrics(string memory _package)
+        public
+        override
+        view
+        returns (address)
+    {
+        return metrics[createKey(_package)];
+    }
 
-	function done() public onlyOwner {
-		migratable = false;
-	}
+    function migrate(
+        address _property,
+        string memory _package,
+        address _market
+    ) public onlyOwner {
+        bytes32 key = createKey(_package);
+        require(migratable, "now is not migratable");
+        register(key, _property, _package, _market);
+    }
 
-	function setKhaos(address _khaos) external onlyOwner {
-		khaos = _khaos;
-	}
+    function done() public onlyOwner {
+        migratable = false;
+    }
+
+    function setKhaos(address _khaos) external onlyOwner {
+        khaos = _khaos;
+    }
 }
-
-
-
-
 
 //https://discordapp.com/channels/547215761341546497/699186073808666646/743340767887622195
 //https://discordapp.com/channels/547215761341546497/699186073808666646/742363946941677639
