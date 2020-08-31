4450b15ed3dab4096cc6b54249241d71327f40f7
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-08-31 10:16:51 +0900

    use dotenv

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable functional/functional-parameters */
 /* eslint-disable functional/no-expression-statement */
 
 import {ethers} from "ethers";
 import * as githubmarket from "./../build/GitHubMarket.json";
+require("dotenv").config();
 
 const deploy = async (): Promise<void> => {
-  /////////////////////////////////////
-  const network = "";
-  const infuraId = "";
-  const mnemonic = "";
-  /////////////////////////////////////
-
-  const provider = ethers.getDefaultProvider(network, {
-    infura: infuraId,
+  const {NETWORK, INFURA_ID, MNEMONIC} = process.env;
+  console.log(`network:${NETWORK}`);
+  console.log(`infura id:${INFURA_ID}`);
+  console.log(`mnemonic:${MNEMONIC}`);
+  const provider = ethers.getDefaultProvider(NETWORK, {
+    infura: INFURA_ID,
   });
-  const wallet = ethers.Wallet.fromMnemonic(mnemonic).connect(provider);
+  const wallet = ethers.Wallet.fromMnemonic(MNEMONIC!).connect(provider);
   const factory = new ethers.ContractFactory(
     githubmarket.abi,
     githubmarket.bytecode,
