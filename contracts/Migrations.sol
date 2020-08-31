a1f82a2a704742388fa77c0684514ec03e00365c
aggre <hiroyuki.aggre@gmail.com>
2020-08-31 11:19:35 +0900

    change to fixed compiler version for security purposes

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-pragma solidity ^0.6.0;
+pragma solidity 0.6.12;
 
 contract Migrations {
     address public owner;
--- contracts/test/MockMarket.sol
+++ contracts/test/MockMarket.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-pragma solidity ^0.6.0;
+pragma solidity 0.6.12;
 import {IMarket} from "contracts/market/GitHubMarket.sol";
 
 contract MockMarket is IMarket {
